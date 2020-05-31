ce8cc93d52017e3dd62b032fa5ccc5034c57691f
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2020-05-31 10:23:51 -0400

    testing aperture selection

--- tests/test_cpm_tools.py
+++ tests/test_cpm_tools.py
@@ -11,13 +11,13 @@ from astroquery.mast import Tesscut, TesscutClass
 def test_download_make_and_stitch():
     # ticid = "164668179"
     # ticid = 48504458
-    ticid = 270608461
+    # ticid = 270608461
+    ticid = 176954932
     tr.download_tess_cuts(ticid, upper_sector_limit=16)
 
     # Create CPM light curve
     tesscut_path="/Users/rangus/projects/TESS-rotation/data/TESScut"
     time_cpm, flux_cpm, sectors = tr.CPM_recover(ticid, tesscut_path,
-                                                any_observed_sector=14,
                                                 upper_sector_limit=16)
 
     # Stitch light curve
@@ -27,4 +27,16 @@ def test_download_make_and_stitch():
     plt.errorbar(time, flux, yerr=flux_err, fmt="k.", alpha=.5)
     plt.savefig("test")
 
+
+def test_get_CPM_aperture():
+    aperture = np.array([[0, 0, 0],
+                         [0, 1, 0],
+                         [0, 0, 0]])
+
+    collims, rowlims = tr.get_CPM_aperture(aperture, xstart=0, ystart=0)
+    assert len(collims) == 2
+    assert len(rowlims) == 2
+
+
 test_download_make_and_stitch()
+# test_get_CPM_aperture()
